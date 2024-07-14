a819db3b0bad283478dcf43a6360d1617176026c
arpachuilo <andrew.pachuilo@gmail.com>
2024-07-13 20:03:52 -0400

    fix for clicking new game link

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -5,6 +5,9 @@ import ENV from 'tic-tac-toe/config/environment';
 export default class NavBarComponent extends Component {
   @action
   reload() {
+    // allow redirect to happen if not already at index
+    if (window.location.pathname !== '/') return;
+
     // test component gets very upset if LinkTo force refreshes
     if (ENV.environment !== 'test') location.reload();
   }
